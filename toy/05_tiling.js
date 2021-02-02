0642fd583d17217169c6ec33c5d2795e1850628e
osunguk(mac) <osunguk@gmail.com>
2021-02-02 09:39:56 +0900

    done 05

--- toy/05_tiling.js
+++ toy/05_tiling.js
@@ -5,22 +5,30 @@ let tiling = function (n) {
 
   // 남은 길이 넘겨준다
   const jegwi = function (len) {
+    console.log(len);
     console.log(++count);
-    if (len === 2) {
-      cnt += 2;
-      return;
+    console.log(solution);
+    console.log(cnt);
+    // if (len === 2) {
+    //   cnt += 2;
+    //   return;
+    // }
+    // if (len === 1) {
+    //   cnt++;
+    //   return;
+    // }
+    if (solution[len - 1] === undefined) {
+      jegwi(len - 1);
+      jegwi(len - 2);
+      solution[len - 1] = cnt;
+    } else {
+      cnt += solution[len - 1];
     }
-    if (len === 1) {
-      cnt++;
-      return;
-    }
-
-    jegwi(len - 1);
-    jegwi(len - 2);
   };
 
   jegwi(n);
+  console.log(solution);
   return cnt;
 };
 
-console.log(tiling(5));
+console.log(tiling(15));
