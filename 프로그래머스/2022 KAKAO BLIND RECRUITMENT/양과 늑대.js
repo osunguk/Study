99fa2a114d5fc99608c9d6c1f98765045ff4f8ea
sung - kook oh <osunguk@gmail.com>
2022-06-20 18:25:36 +0900

    양과 눅대 문제 주석 추가

--- 프로그래머스/2022 KAKAO BLIND RECRUITMENT/양과 늑대.js
+++ 프로그래머스/2022 KAKAO BLIND RECRUITMENT/양과 늑대.js
@@ -22,10 +22,28 @@ function solution(info, edges) {
       };
     }
   });
+  /*  아래와 같은 형식으로 데이터를 만든다
+  { 0: { species: 'sheep', chilNode: [ 1, 8 ] },
+  1: { species: 'sheep', chilNode: [ 2, 4 ] },
+  2: { species: 'wolf', chilNode: [] },
+  3: { species: 'wolf', chilNode: [] },
+  4: { species: 'wolf', chilNode: [ 3, 6 ] },
+  5: { species: 'sheep', chilNode: [] },
+  6: { species: 'wolf', chilNode: [ 5 ] },
+  7: { species: 'sheep', chilNode: [] },
+  8: { species: 'wolf', chilNode: [ 7, 9 ] },
+  9: { species: 'sheep', chilNode: [ 10, 11 ] },
+  10: { species: 'wolf', chilNode: [] },
+  11: { species: 'wolf', chilNode: [] } }
+  */
 
   let max = 0;
 
   const recursion = (sheepCount, wolfCount, position, canGo) => {
+    // sheepCount - 양 숫자
+    // wolfCount - 늑대 숫자
+    // position - 현재 위치
+    // canGo - 방문 할 수 있는 장소를 모두 모음
     if (max < sheepCount) {
       max = sheepCount;
     }
@@ -62,28 +80,28 @@ function solution(info, edges) {
   return max;
 }
 
-// solution(
-//   [0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
-//   [
-//     [0, 1],
-//     [1, 2],
-//     [1, 4],
-//     [0, 8],
-//     [8, 7],
-//     [9, 10],
-//     [9, 11],
-//     [4, 3],
-//     [6, 5],
-//     [4, 6],
-//     [8, 9],
-//   ]
-// );
-// 5
-
 solution(
-  [0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
   [
     [0, 1],
-    [0, 2],
+    [1, 2],
+    [1, 4],
+    [0, 8],
+    [8, 7],
+    [9, 10],
+    [9, 11],
+    [4, 3],
+    [6, 5],
+    [4, 6],
+    [8, 9],
   ]
 );
+// 5
+
+// solution(
+//   [0, 0, 0],
+//   [
+//     [0, 1],
+//     [0, 2],
+//   ]
+// );
